e19be5cb371e4001ee528f755c9d11deba7edeba
damiendeboudt <damien.deboudt@sfr.fr>
2022-12-07 22:20:34 +0100

    reste boutton stop

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,21 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
+let compteur = 0
 let countUp = function() {
-    // ?
+    setTimeout(function () {
+        compteur++
+      seconds.innerHTML = compteur.toString()
+    countUp()
+    }, 1000)
 };
+countUp()
+
+
+
 
 // How can you make it stop counting?
 let stopCountUp = function() {
-};
+}
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
 
